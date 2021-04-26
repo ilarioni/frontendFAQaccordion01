515fa9bbf8b185394ad4461a505d619db357befb
ilarioni <ilia.meshvildishvili@gmail.com>
2021-04-26 15:29:07 +0200

    js accordion script

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ document.querySelectorAll('.accordion__question').forEach((item) => {
     item.addEventListener('click', (event) => {
         console.log('clock!');
         item.classList.toggle('open');
+        item.nextElementSibling.classList.toggle('open');
     });
 });
