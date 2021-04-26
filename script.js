8f8e23403d762f3c96d714a8ad768bf9b195c605
ilarioni <ilia.meshvildishvili@gmail.com>
2021-04-26 15:14:30 +0200

    js query selector
    bold and rotate

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+document.querySelectorAll('.accordion__question').forEach((item) => {
+    item.addEventListener('click', (event) => {
+        console.log('clock!');
+        item.classList.toggle('open');
+    });
+});
